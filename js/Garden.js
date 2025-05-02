6a7dea6ab7bfba476fc6c9e630d56e5f7932be28
Athanasios Papapetrou <athanasios.papapetrou@netcompany.com>
2025-05-02 17:00:52 +0300

    Altitudes

--- js/Garden.js
+++ js/Garden.js
@@ -28,37 +28,14 @@ class Garden {
             this.#renderer = await new Renderer(this.#gl, Garden.#SHADER_VERTEX, Garden.#SHADER_FRAGMENT,
                     Garden.#UNIFORMS, Garden.#ATTRIBUTES);
             const garden = await(await GardenRacers.load(url)).json();
-            const positions = [];
-            for (let latitude = 0; latitude < 2 * garden.latitude + 1; latitude++) {
-                for (let longitude = 0; longitude < 2 * garden.longitude + 1; longitude++) {
-                    positions.push(longitude / 2, 0.0, -latitude / 2);
-                }
-            }
-            const indices = [];
-            const n = 2 * garden.longitude + 1;
-            for (let latitude = 0; latitude < garden.latitude; latitude++) {
-                for (let longitude = 0; longitude < garden.longitude; longitude++) {
-                    const lat = 2 * latitude + 1;
-                    const long = 2 * longitude + 1;
-                    const north = Direction.N(lat, long).lat * n + Direction.N(lat, long).long;
-                    const northEast = Direction.NE(lat, long).lat * n + Direction.NE(lat, long).long;
-                    const center = lat * n + long;
-                    for (let direction = 0; direction < Object.keys(Direction).length; direction++) {
-                        const dir = Object.values(Direction)[direction](lat, long);
-                        const nextDir = Object.values(Direction)[(direction + 1) % Object.keys(Direction).length](lat, long);
-                        indices.push(dir.lat * n + dir.long,
-                            lat * n + long,
-                            nextDir.lat * n + nextDir.long
-                        );
-                    }
-                }
-            }
+
             this.#array = this.#gl.createVertexArray();
             this.#gl.bindVertexArray(this.#array);
-            new RenderingData(this.#gl, this.#gl.ARRAY_BUFFER, new Float32Array(positions));
+            new RenderingData(this.#gl, this.#gl.ARRAY_BUFFER, new Float32Array(this.#positions(garden)));
             this.#gl.vertexAttribPointer(this.#renderer.attributes.position, 3, gl.FLOAT, false, 0, 0);
             this.#gl.enableVertexAttribArray(this.#renderer.attributes.position);
             // TODO refactor
+            const indices = this.#indices(garden);
             new RenderingData(this.#gl, this.#gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(indices));
             this.#gl.bindVertexArray(null);
             this.#count = indices.length;
@@ -77,5 +54,52 @@ class Garden {
         // TODO render
         this.#gl.bindVertexArray(this.#array);
         this.#gl.drawElements(this.#gl.TRIANGLES, this.#count, this.#gl.UNSIGNED_SHORT, 0);
+    }
+
+    #positions(garden) {
+        const positions = [];
+        for (let latitude = 0; latitude < 2 * garden.latitude + 1; latitude++) {
+            for (let longitude = 0; longitude < 2 * garden.longitude + 1; longitude++) {
+                const lat = Math.floor((latitude - 1) / 2);
+                const long = Math.floor((longitude - 1) / 2);
+                let altitude = 0.0;
+                if ((latitude % 2 == 1) && (longitude % 2 == 1)) { // center
+                    altitude = garden.altitudes[lat * garden.longitude + long];
+                } else if (latitude % 2 == 1) { // average east and west
+                    altitude = (garden.altitudes[lat * garden.longitude + Math.min(long + 1, garden.longitude - 1)]
+                            + garden.altitudes[lat * garden.longitude + Math.max(long, 0)]) / 2.0;
+                } else if (longitude % 2 == 1) { // average north and south
+                    altitude = (garden.altitudes[Math.min(lat + 1, garden.latitude - 1) * garden.longitude + long]
+                            + garden.altitudes[Math.max(lat, 0) * garden.longitude + long]) / 2.0;
+                } else { // average all directions
+                    altitude = (garden.altitudes[Math.min(lat + 1, garden.latitude - 1) * garden.longitude + Math.max(long, 0)]
+                            + garden.altitudes[Math.max(lat, 0) * garden.longitude + Math.min(long + 1, garden.longitude - 1)]
+                            + garden.altitudes[Math.max(lat, 0) * garden.longitude + Math.max(long, 0)]
+                            + garden.altitudes[Math.max(lat, 0) * garden.longitude + Math.max(long, 0)]) / 4.0;
+                }
+                positions.push(longitude / 2.0, altitude, -latitude / 2.0);
+            }
+        }
+        return positions;
+    }
+
+    #indices(garden) {
+        const indices = [];
+        const n = 2 * garden.longitude + 1;
+        for (let latitude = 0; latitude < garden.latitude; latitude++) {
+            for (let longitude = 0; longitude < garden.longitude; longitude++) {
+                const lat = 2 * latitude + 1;
+                const long = 2 * longitude + 1;
+                for (let direction = 0; direction < Object.keys(Direction).length; direction++) {
+                    const dir = Object.values(Direction)[direction](lat, long);
+                    const nextDir = Object.values(Direction)[(direction + 1) % Object.keys(Direction).length](lat, long);
+                    indices.push(dir.lat * n + dir.long,
+                        lat * n + long,
+                        nextDir.lat * n + nextDir.long
+                    );
+                }
+            }
+        }
+        return indices;
     }
 }
