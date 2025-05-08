c7033a125b1f510698079055725781f372aac2e2
Athanasios Papapetrou <athanasios.papapetrou@netcompany.com>
2025-05-08 16:19:57 +0300

    Cleanup

--- js/rendering/Texture.js
+++ js/rendering/Texture.js
@@ -1,20 +1,25 @@
 'use strict';
 
 class Texture {
+    static #ERROR_LOADING = (url) => `Error loading ${url}`;
+
     #texture;
 
     constructor(gl, url) {
         return (async () => {
-            // TODO cleanup
-            var image = new Image();
-            image.onload = () => {
-                this.#texture = gl.createTexture();
-                gl.activeTexture(gl.TEXTURE0 + 0);
-                gl.bindTexture(gl.TEXTURE_2D, this.#texture); // TODO unbound texture
-                gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA,gl.UNSIGNED_BYTE, image);
-                gl.generateMipmap(gl.TEXTURE_2D);
-            };
-            image.src = url;
+            const image = await new Promise((resolve) => {
+                const image = new Image();
+                image.onload = () => resolve(image);
+                image.onerror = () => {
+                    throw new Error(Texture.#ERROR_LOADING(url)); // TODO rejections
+                };
+                image.src = url;
+            });
+            this.#texture = gl.createTexture();
+            gl.activeTexture(gl.TEXTURE0 + 0);
+            gl.bindTexture(gl.TEXTURE_2D, this.#texture); // TODO unbound texture
+            gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA,gl.UNSIGNED_BYTE, image);
+            gl.generateMipmap(gl.TEXTURE_2D);
             return this;
         })();
     }
