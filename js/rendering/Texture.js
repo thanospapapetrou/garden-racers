26e55c23dd4c2640b9cbd5876487b78a3e90d3c2
Athanasios Papapetrou <athanasios.papapetrou@netcompany.com>
2025-05-08 20:08:13 +0300

    Cleanup

--- js/rendering/Texture.js
+++ js/rendering/Texture.js
@@ -18,17 +18,13 @@ class Texture {
             this.#unit = unit;
             const texture = gl.createTexture();
             gl.activeTexture(this.#unit);
-            gl.bindTexture(gl.TEXTURE_2D, texture); // TODO unbound texture
-//            gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
-            // TODO
-//                gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
-//                gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
-//                gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+            gl.bindTexture(gl.TEXTURE_2D, texture);
+            gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+            gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+            gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
+            gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR_MIPMAP_LINEAR);
             gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA,gl.UNSIGNED_BYTE, image);
             gl.generateMipmap(gl.TEXTURE_2D);
-//            gl.activeTexture(0);
-//            gl.bindTexture(gl.TEXTURE_2D, null); // TODO unbound texture
-////            gl.activeTexture(gl.TEXTURE2);
             return this;
         })();
     }
